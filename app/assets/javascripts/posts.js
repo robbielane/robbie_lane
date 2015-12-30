7eae9cdd1f62cb5132be352a7cac7a51a02e1c34
Robbie Lane <lane.robbie@gmail.com>
2015-12-30 12:34:49 -0600

    Integration works, still have to get links to work

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,15 +1,14 @@
-// $(document).ready(function() {
-//   $.ajax({
-//     type: 'GET',
-//     url: '/api/v1/posts.json',
-//     success: function(posts) {
-//       $.each(posts, function(index, post) {
-//         $('.posts').append(
-//           "<div class='post clickable' data-id=" +
-//           post.id + "><h5>" + post.title + "</h5>" +
-//           "<p>" + post.body + "</p></div>"
-//         )
-//       })
-//     }
-//   });
-// });
+$(document).ready(function() {
+  $.ajax({
+    type: 'GET',
+    url: 'http://localyost:3000/api/v1/recent.json',
+    success: function(posts) {
+      $.each(posts, function(index, post) {
+        $('.photos').append(
+          "<div id='photo_" + index + "' class='photo clickable' data-id=''" + "><h5>" + post.name + "</h5>" + "<p>" + post.description + "</p></div>"
+        )
+        $("#photo_" + index).css("background-image", "url(" + post.h_thumb_url + ")")
+      })
+    }
+  });
+});
