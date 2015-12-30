d9a7a99e691b38910e60c40bf14a69c50c4cffcc
Robbie Lane <lane.robbie@gmail.com>
2015-12-30 13:24:34 -0600

    links now working for photo elements

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -5,9 +5,13 @@ $(document).ready(function() {
     success: function(posts) {
       $.each(posts, function(index, post) {
         $('.photos').append(
-          "<div id='photo_" + index + "' class='photo clickable' data-id=''" + "><h5>" + post.name + "</h5>" + "<p>" + post.description + "</p></div>"
+          "<div id='photo_" + index + "' class='photo clickable'" + "><h5>" + post.name + "</h5>" + "<p>" + post.description + "</p></div>"
         )
         $("#photo_" + index).css("background-image", "url(" + post.h_thumb_url + ")")
+        $("#photo_" + index).attr("data-link", "http://localyost:3000" + post.picture_uri)
+        $('.photos').on('click', function() {
+          window.document.location = $(this).data('link');
+        });
       })
     }
   });
