219cc3cf8ae0dbd349a8134c4cb369169a757d76
Robbie Lane <lane.robbie@gmail.com>
2015-12-30 18:43:20 -0600

    update ajax call for new domian

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
   $.ajax({
     type: 'GET',
-    url: 'http://localyost:3000/api/v1/recent.json',
+    url: 'http://photography.robbielane.me/api/v1/recent.json',
     success: function(posts) {
       $.each(posts, function(index, post) {
         $('.photos').append(
-          "<div id='photo_" + index + "' class='photo clickable'" + "><h5>" + post.name + "</h5>" + "<p>" + post.description + "</p></div>"
+          "<div id='photo_" + index + "' class='photo'" + "><h5>" + post.name + "</h5>" + "<p>" + post.description + "</p></div>"
         )
         $("#photo_" + index).css("background-image", "url(" + post.h_thumb_url + ")")
-        $("#photo_" + index).attr("data-link", "http://localyost:3000" + post.picture_uri)
+        $("#photo_" + index).attr("data-link", "http://photography.robbielane.me" + post.picture_uri)
         $('.photos').on('click', function() {
-          window.document.location = $(this).data('link');
+          window.document.location = "http://photography.robbielane.me" + post.picture_uri
         });
       })
     }
