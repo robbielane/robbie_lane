3b32a41984b36f958cde2f69996dcbfe3e029f87
Robbie Lane <lane.robbie@gmail.com>
2016-01-02 17:05:40 -0700

    change data-link click method

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -7,11 +7,11 @@ $(document).ready(function() {
         $('.posts').append(
           "<div id='photo_" + index + "' class='photo'" + "><h5>" + post.name + "</h5>" + "<p>" + post.description + "</p></div>"
         )
-        $("#photo_" + index).css("background-image", "url(" + post.h_thumb_url + ")")
+        $("#photo_" + index ).css("background-image", "url(" + post.h_thumb_url + ")")
         $("#photo_" + index).attr("data-link", "http://photography.robbielane.me" + post.picture_uri)
-        $("#photo_" + index).attr("data-timestamp", "post.created_at")
+        $("#photo_" + index).attr("data-timestamp", post.created_at)
         $('.photo').on('click', function() {
-          window.document.location = "http://photography.robbielane.me" + post.picture_uri
+          window.document.location = $(this).data('link');
         });
       })
     }
