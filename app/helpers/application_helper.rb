70dfb1cbd7e891d5f10ff682dc556f8af0f0b8ed
Robbie Lane <lane.robbie@gmail.com>
2016-02-06 16:17:20 -0700

    use coderay to enable syntax highlighting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,27 @@
 module ApplicationHelper
 
+  class CodeRayify < Redcarpet::Render::HTML
+    def block_code(code, language)
+      CodeRay.scan(code, language).div
+    end
+  end
+
   def markdown(text)
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
-      no_intra_emphasis: true,
-      fenced_code_blocks: true,
-      disable_indented_code_blocks: true)
-    return markdown.render(text).html_safe
+    # markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+    #   no_intra_emphasis: true,
+    #   fenced_code_blocks: true,
+    #   disable_indented_code_blocks: true)
+    # return markdown.render(text).html_safe
+    coderayified = CodeRayify.new(:filter_html => true, :hard_wrap => true)
+
+    options = {
+      :fenced_code_blocks => true,
+      :no_intra_emphasis => true,
+      :autolink => true,
+      :lax_html_blocks => true,
+    }
+    markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+    markdown_to_html.render(text).html_safe
   end
 
   def svg_tag filename, options={}
