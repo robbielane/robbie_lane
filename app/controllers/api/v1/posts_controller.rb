bec673d3caffd7f33dcc2740ae188fd8053fea20
Robbie Lane <lane.robbie@gmail.com>
2016-03-08 14:25:17 -0700

    create api_key model

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,9 +1,22 @@
 class Api::V1::PostsController < Api::V1::BaseController
+  before_filter :restrict_access, only: [:create]
+
   def index
     respond_with Post.all
   end
 
   def show
     respond_with Post.find(params[:id])
+  end
+
+  def create
+    
+  end
+
+  private
+
+  def restrict_access
+    api_key = ApiKey.find_by(access_token: params[:access_token])
+    head :unauthorized unless api_key
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :posts, only: [:show, :index]
+      resources :posts, only: [:show, :index, :create]
     end
   end
 end
